272722f45f2cc1ff1526c2b6b0b5092c540e863e
erick1984linares <56937073+erick1984linares@users.noreply.github.com>
2019-11-23 08:04:50 -0500

    se ha terminado el decodificador 2

---
+++ linares/Bucles_iteracion_en_rango/recorrido2.py
@@ -0,0 +1,10 @@
+# Imprimir los "M" primeros numeros divididos entre 2
+import os
+#input
+M=int(os.sys.argv[1])
+#iterador
+for i in range(M):
+    print(i/2)
+#fin_iterador_en_rango
+
+print("Fin del bucle")
