3a7698ff0e0741acb6678a64f35617d7bc4a49bb
erick1984linares <56937073+erick1984linares@users.noreply.github.com>
2019-11-23 11:07:20 -0500

    Create lote.bat

---
+++ lote.bat
@@ -0,0 +1,48 @@
+python linares/Bucles_mientras/validador1.py 5 1 5
+python linares/Bucles_mientras/validador2.py 100 5
+python linares/Bucles_mientras/validador3.py 20 10
+python linares/Bucles_mientras/validador4.py 1000 9
+python linares/Bucles_mientras/validador5.py 3.14 5 6
+
+python linares/Bucles_para/recorrido1.py 500
+python linares/Bucles_para/recorrido2.py 55 125
+python linares/Bucles_para/recorrido3.py 30 90
+python linares/Bucles_para/recorrido4.py -30 30
+python linares/Bucles_para/recorrido5.py 200 1350
+
+python linares/Bucles_iteracion_en_rango/recorrido1.py 100
+python linares/Bucles_iteracion_en_rango/recorrido2.py 200
+python linares/Bucles_iteracion_en_rango/recorrido3.py 100 200
+python linares/Bucles_iteracion_en_rango/recorrido4.py 100
+python linares/Bucles_iteracion_en_rango/recorrido5.py 135
+
+python linares/Bucles_iteracion/decodificador1.py 1234
+python linares/Bucles_iteracion/decodificador2.py ABFRMPQS
+python linares/Bucles_iteracion/decodificador3.py ABFR
+python linares/Bucles_iteracion/decodificador4.py 9876
+python linares/Bucles_iteracion/decodificador5.py CPSH
+
+#lote.bat de mi compañero
+python Rojas/While/ejercicio01.py 2019
+python Rojas/While/ejercicio02.py 524
+python Rojas/While/ejercicio03.py Matematica
+python Rojas/While/ejercicio04.py 18
+python Rojas/While/ejercicio05.py 10
+
+python Rojas/Range/ejercicio01.py 0 10
+python Rojas/Range/ejercicio02.py 100 15
+python Rojas/Range/ejercicio03.py 2 16
+python Rojas/Range/ejercicio04.py 100
+python Rojas/Range/ejercicio05.py 10
+
+python Rojas/para/ejercicio01.py 100
+python Rojas/para/ejercicio02.py 20 80
+python Rojas/para/ejercicio03.py 120 200
+python Rojas/para/ejercicio04.py 135
+python Rojas/para/ejercicio05.py 100 660
+
+python Rojas/iteracion/ejercicio01.py 10203040
+python Rojas/iteracion/ejercicio02.py abcdef
+python Rojas/iteracion/ejercicio03.py ABFR
+python Rojas/iteracion/ejercicio04.py 1234
+python Rojas/iteracion/ejercicio05.py MNP
