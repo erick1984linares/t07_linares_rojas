6e34b7a8ff7c1a418f548f4e93574b3cc51de1b1
erick1984linares <56937073+erick1984linares@users.noreply.github.com>
2019-11-23 07:46:12 -0500

    se ha terminado el decodificador 5

---
+++ rojas/Bucles_iteracion/decodificador5.py
@@ -0,0 +1,19 @@
+# Decodificar mensaje encriptado
+# C = Chompas
+# P = Polos
+# S = Shorts
+# H = Chalecos
+msg="CPSHSPH"
+
+for letra in msg:
+    if letra == "C":
+        print("Chompas")
+    if letra == "P":
+        print("Polos")
+    if letra == "S":
+        print("Shorts")
+    if letra == "H":
+        print("Chalecos")
+#fin_iterador
+
+print("Fin del bucle")
