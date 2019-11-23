d48c49609a2351cc624913dcf85e6c6970e3f883
erick1984linares <56937073+erick1984linares@users.noreply.github.com>
2019-11-23 08:05:17 -0500

    se ha terminado y agregado el recorrido 4

---
+++ linares/Bucles_iteracion_en_rango/recorrido4.py
@@ -0,0 +1,10 @@
+# Imprimir los "L" primeros numeros multiplicados por 2 y divididos entre 5
+import os
+#input
+L=int(os.sys.argv[1])
+#iterador
+for i in range(L):
+    print((i)*2/5)
+#fin_iterador_en_rango
+
+print("Fin del bucle")
