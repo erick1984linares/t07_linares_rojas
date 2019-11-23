4f7435c20905091e8e658ee8e9443ecca8a8361d
erick1984linares <56937073+erick1984linares@users.noreply.github.com>
2019-11-23 08:07:33 -0500

    se ha terminado y agregado el validador 1

---
+++ linares/Bucles_mientras/validador1.py
@@ -0,0 +1,23 @@
+import os
+#DECLARAR VARIABLES
+largo, ancho, altura=0, 0, 0
+
+#imput
+largo=int(os.sys.argv[1])
+ancho=int(os.sys.argv[2])
+altura=int(os.sys.argv[3])
+
+#procesing
+volumen_invalido=(largo*ancho*altura)
+
+#Verificador
+volumen=(volumen < 24 or volumen > 60 )
+
+#VALIDAR EL VOLUMEN DE UN RECTOEDRO
+volumen_invalido=True
+while (volumen_invalido):
+    volumen=int(input("ingrese volumen:"))
+    volumen_invalido=(volumen < 24 or volumen > 60  )
+#fin_while
+print("fin del bucle")
+
