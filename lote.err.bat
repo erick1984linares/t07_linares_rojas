b5027e809e8f9143d548fdf3b364eec457b80bdb
erick1984linares <56937073+erick1984linares@users.noreply.github.com>
2019-11-23 11:07:44 -0500

    Create lote.err.bat

---
+++ lote.err.bat
@@ -0,0 +1,48 @@
+python linares/Bucles_mientras/validador1.py 3.4 5.8 2
+python linares/Bucles_mientras/validador2.py 201.3 10
+python linares/Bucles_mientras/validador3.py 21.4 5
+python linares/Bucles_mientras/validador4.py 1000 9.5
+python linares/Bucles_mientras/validador5.py 3.14 13.2 5
+
+python linares/Bucles_para/recorrido2.py -100
+python linares/Bucles_para/recorrido2.py 40 60
+python linares/Bucles_para/recorrido2.py 20 -100
+python linares/Bucles_para/recorrido2.py 30 -30
+python linares/Bucles_para/recorrido2.pyv 100 20
+
+python linares/Bucles_iteracion_en_rango/recorrido1.py 101.5
+python linares/Bucles_iteracion_en_rango/recorrido2.py 200.98
+python linares/Bucles_iteracion_en_rango/recorrido3.py 98.5 50
+python linares/Bucles_iteracion_en_rango/recorrido4.py 90.60
+python linares/Bucles_iteracion_en_rango/recorrido5.py 135.98
+
+python linares/Bucles_iteracion/decodificador1.py abcd
+python linares/Bucles_iteracion/decodificador2.py 123456
+python linares/Bucles_iteracion/decodificador3.py 1234
+python linares/Bucles_iteracion/decodificador4.py abcd
+python linares/Bucles_iteracion/decodificador5.py ABCD
+
+#lote.err.bat de mi compañero
+python Rojas/While/ejercicio01.py 2019
+python Rojas/While/ejercicio02.py 524
+python Rojas/While/ejercicio03.py Matematica
+python Rojas/While/ejercicio04.py 18
+python Rojas/While/ejercicio05.py 10
+
+python Rojas/Range/ejercicio01.py 0 10
+python Rojas/Range/ejercicio02.py 100 15
+python Rojas/Range/ejercicio03.py 2 16
+python Rojas/Range/ejercicio04.py 100
+python Rojas/Range/ejercicio05.py 19.54
+
+python Rojas/para/ejercicio01.py a
+python Rojas/para/ejercicio02.py 120.64
+python Rojas/para/ejercicio03.py 90 a
+python Rojas/para/ejercicio04.py 135+a
+python Rojas/para/ejercicio05.py 100+a 200+b
+
+python Rojas/iteracion/ejercicio01.py abcdef
+python Rojas/iteracion/ejercicio02.py 123456
+python Rojas/iteracion/ejercicio03.py 1234
+python Rojas/iteracion/ejercicio04.py pqrs
+python Rojas/iteracion/ejercicio05.py 123
